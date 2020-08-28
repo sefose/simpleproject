1564a439eeaf6e36a420f9767c8240219b8b4469
Thodryth <thodryth@gmx.net>
2020-08-28 13:09:48 +0200

    deleted wrong stuff

--- simplespring/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `poll` (`id`, `question`, `token`) 
-VALUES ('1', 'Chris ist cool!', '1234');
-
-INSERT INTO `poll` (`id`, `question`, `token`) 
-VALUES ('2', 'Chris ist cool!', '1234');
-
-INSERT INTO `poll` (`id`, `question`, `token`) 
-VALUES ('3', 'Chris ist cool!', '1234');

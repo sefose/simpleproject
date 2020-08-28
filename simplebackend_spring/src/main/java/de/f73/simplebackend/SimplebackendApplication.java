5bbeb1772be1003e82d5afaf78a17965a9f66c6c
Robert Rudolph <robert@deepthought.local>
2020-08-28 13:16:13 +0200

    added spring backend

---
+++ simplebackend_spring/src/main/java/de/f73/simplebackend/SimplebackendApplication.java
@@ -0,0 +1,13 @@
+package de.f73.simplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebackendApplication.class, args);
+	}
+
+}
---
+++ simplebackend_spring/src/test/java/de/f73/simplebackend/SimplebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.f73.simplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

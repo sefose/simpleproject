0a7bff325d208f4c86d808c39241f04c5f97dfa1
Robert Rudolph <robert@deepthought.local>
2020-08-29 11:53:53 +0200

    can create dummies

--- frontend/src/views/DummyCreator.jsx
+++ frontend/src/views/DummyCreator.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { saveDummy } from "../service/DummyService";
 
 function DummyCreator() {
-    return (
-        <div>
-            create your Dummy!
-        </div>
-    )
+
+    const [name, setName] = useState("");
+
+    const submit = () => {
+        saveDummy({name})
+    }
+
+  return (
+    <Form >
+        <Form.Group controlId="formGroupName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter Dummy name" value={name} onChange={e => setName(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={submit}>Create</Button>
+    </Form>
+    );
 }
 
-export default DummyCreator
+export default DummyCreator;
